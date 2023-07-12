407096a1cb12d99aa1f3d52a587d2ec2551bab7a
Sergey <shkaev.ss@mail.ru>
2023-07-12 17:42:03 +0300

    metanit ex2

--- Metanit/src/ex1.java
+++ Metanit/src/ex1.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+
 /*
 Упражнение 1
 Напишите консольную программу, в которой пользователь с клавиатуры вводит два числа.
@@ -8,15 +9,15 @@ import java.util.Scanner;
  */
 public class ex1 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Введите первое число: ");
         int a = sc.nextInt();
 
         System.out.print("Введите второе число: ");
         int b = sc.nextInt();
 
-        if(a>b) System.out.printf("%d > %d", a, b);
-        else  if(a<b) System.out.printf("%d > %d", b, a);
+        if (a > b) System.out.printf("%d > %d", a, b);
+        else if (a < b) System.out.printf("%d > %d", b, a);
         else System.out.printf("%d = %d", a, b);
         System.out.println();
 
